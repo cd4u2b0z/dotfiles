bcbbbc091c14c0c7ccaab16fe0267a2273b52cfe
craig <craig@localhost>
2026-01-14 04:55:52 -0500

    Remove 53 unused files: old scripts, cava shaders, btop nord theme, docs

--- dot_config/waybar/scripts/executable_optimized-app-tray.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-echo '{"text": "󰓇", "tooltip": "Spotify (ncspot) - Right click to kill", "class": "running"}'
--- dot_config/waybar/scripts/executable_system-stats-wrapper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Get system stats tooltip from your existing script  
-stats=$(~/.config/waybar/scripts/system-monitor.sh 2>/dev/null | jq -r '.tooltip')
-
-# Output JSON with microchip icon and tooltip containing the stats
-echo "{\"text\":\"󰍛\",\"tooltip\":\"$stats\"}"
